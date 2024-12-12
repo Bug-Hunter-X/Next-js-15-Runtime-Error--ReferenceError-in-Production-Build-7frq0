90924276862d812247e55a10f9cdde250a6bd639
BugHunter-X <askzenai@gmail.com>
2024-12-12 09:46:06 +0100

    Add aboutSolution.js file

---
+++ aboutSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// pages/aboutSolution.js
+
+export default function About() {
+  const message = 'About Page';
+  console.log(message);
+  return (
+    <div>{message}</div>
+  );
+}
+```
